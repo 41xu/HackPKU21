2948b00d58cb93e93c83406bd2892d84e0bd1ed8
santiweide <ggy_778@sina.com>
2021-05-10 08:20:07 +0800

    add secret

--- src/main/java/com/jimo/controller/ManagerController.java
+++ src/main/java/com/jimo/controller/ManagerController.java
@@ -4,60 +4,177 @@ package com.jimo.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.jimo.model.common.Result;
-import com.jimo.utils.MyConsts;
-import com.jimo.utils.SignatureGenUtils;
-import com.jimo.utils.TimeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Date;
-import java.util.TreeMap;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 /**
  * 运维接口
  *
  * @author algorithm
- * @href https://docsv3.qingcloud.com/development_docs/api/command_list/rdb/stop_rdbs/
  */
 @RestController
-@RequestMapping("/rds")
+@RequestMapping("/manager")
 public class ManagerController {
     /**
-     * todo jwt鉴权
-     *
-     * @return
+     *   todo be param
      */
-    @GetMapping("/stop")
-    public Result stopRds(@RequestParam(value = "order", required = false) String order)
-            throws IOException, InterruptedException {
-        if (order == null) {
-            return new Result(200, "invalid request");
+    public static String ACCESS_KEY_ID = "UYAVPRHPJIYRBWGPFCIK";
+    /**
+     * todo be param
+     */
+    public static String SECRET_ACCESS_KEY = "7TybgSeNlptBzo5Iy15PjkeLblNwBfNhD1h8HjxU";
+
+    public static String METHOD = "HmacSHA256";
+
+    public static String formatInstant(Instant instant) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                .withZone(ZoneId.of("UTC"))
+                .format(instant);
+    }
+
+    private static final String HEX = "0123456789ABCDEF";
+
+    public static String encodeURIComponent(String str) {
+        if (str == null) {
+            return null;
         }
-        TreeMap<String, String> params = new TreeMap<>();
-        params.put("access_key_id", MyConsts.ACCESS_KEY_ID);
-        params.put("action", "StopRDBs");
-        params.put("clusters.n", MyConsts.CL_ID);
-        params.put("signature_method", MyConsts.API_SIGNATURE_METHOD);
-        params.put("signature_version", MyConsts.API_SIGNATURE_VERSION);
-        params.put("time_stamp", TimeUtils.getISO8601Timestamp(new Date()));
-        params.put("version", MyConsts.API_VERSION);
-        params.put("zone", MyConsts.RDB_ZONE);
-        String signature = SignatureGenUtils.genSignatureWithParams(params);
-        System.out.println("signature " + signature);
+
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        StringBuilder builder = new StringBuilder(bytes.length);
+
+        for (byte c : bytes) {
+            if (c >= 'a' ? c <= 'z' || c == '~' :
+                    c >= 'A' ? c <= 'Z' || c == '_' :
+                            c >= '0' ? c <= '9' : c == '-' || c == '.') {
+                builder.append((char) c);
+            } else {
+                builder.append('%')
+                        .append(HEX.charAt(c >> 4 & 0xf))
+                        .append(HEX.charAt(c & 0xf));
+            }
+        }
+
+        return builder.toString();
+    }
+
+    public static String createUrl(HashMap<String, String> args) throws Exception {
+        // 1.  升序排列
+        var list = new ArrayList<>(args.keySet());
+        Collections.sort(list);
+
+        // 2. 3. 边编码边生成querystring
+        var sb = new StringBuilder();
+        for (var key : list) {
+            sb.append(encodeURIComponent(key));
+            sb.append("=");
+            sb.append(encodeURIComponent(args.get(key)));
+            sb.append("&");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        // 4. 待签名串
+        var signed = "GET\n/iaas/\n" + sb.toString();
+
+        // 5. 签名
+        Mac mac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_ACCESS_KEY.getBytes(StandardCharsets.UTF_8), METHOD);
+        mac.init(secretKeySpec);
+        var hmacSha256 = mac.doFinal(signed.getBytes(StandardCharsets.UTF_8));
+
+        var base64 = Base64.getEncoder().encodeToString(hmacSha256);
+
+        var urlEncoded = encodeURIComponent(base64);
+
+        // 6. 装在请求串最后面
+        sb.append("&signature=").append(urlEncoded);
+
+        // 7. url
+        var url = "https://api.qingcloud.com/iaas/?" + sb.toString();
+
+        return url;
+    }
+
+
+    @GetMapping("/stop/cl")
+    public Result stopCl() throws Exception {
+
+        var requestArgs = new HashMap<String, String>();
+
+        requestArgs.put("action", "StopClusters");
+        requestArgs.put("clusters.1", "cl-x82wzm1w");
+        requestArgs.put("zone", "sh1a");
+
+        var now = Instant.now();
+
+        requestArgs.put("time_stamp", formatInstant(now));
+
+        requestArgs.put("access_key_id", ACCESS_KEY_ID);
+        requestArgs.put("version", "1");
+        requestArgs.put("signature_version", "1");
+        requestArgs.put("signature_method", METHOD);
+
+        var url = createUrl(requestArgs);
+
+        // 8. call
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest.Builder builder = HttpRequest.newBuilder();
-        for(String key: params.keySet()){
-            builder.setHeader(key, params.get(key));
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        JSONObject response = JSON.parseObject(client.send(request, HttpResponse.BodyHandlers.ofString()).body());
+        return new Result(200, "", response);
+    }
+
+
+    @GetMapping("/stop/cl/v2")
+    public Result stopClV2(@RequestParam(value = "appId", required = true) String appId,
+                          @RequestParam(value = "appSecret", required = true) String appSecret,
+                          @RequestParam(value = "clusterIds", required = true) List<String> clusterIds,
+                          @RequestParam(value = "zone", required = true) String zone) throws Exception {
+        ACCESS_KEY_ID = appId;
+        SECRET_ACCESS_KEY = appSecret;
+
+        var requestArgs = new HashMap<String, String>();
+
+        requestArgs.put("action", "StopClusters");
+        int i = 1;
+        for(String cl:clusterIds){
+            requestArgs.put("clusters."+i, cl);
+            i ++;
         }
-        builder.setHeader("signature", signature);
-        HttpRequest request = builder.uri(URI.create("https://api.qingcloud.com/iaas/")).build();
+        requestArgs.put("zone", zone);
+
+        var now = Instant.now();
+
+        requestArgs.put("time_stamp", formatInstant(now));
+
+        requestArgs.put("access_key_id", ACCESS_KEY_ID);
+        requestArgs.put("version", "1");
+        requestArgs.put("signature_version", "1");
+        requestArgs.put("signature_method", METHOD);
+
+        var url = createUrl(requestArgs);
+
+        // 8. call
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
         JSONObject response = JSON.parseObject(client.send(request, HttpResponse.BodyHandlers.ofString()).body());
         return new Result(200, "", response);
     }
--- src/main/java/com/jimo/utils/SignatureGenUtils.java
+++ src/main/java/com/jimo/utils/SignatureGenUtils.java
@@ -30,7 +30,7 @@ public class SignatureGenUtils {
         }
         String s1 = stringBuilder.toString();
         String strToSign = "GET\\n/iaas/\\n" +s1;
-        //String strToSign = "GET\\n/iaas/\\naccess_key_id=QYACCESSKEYIDEXAMPLE&action=RunInstances&count=1&image_id=centos64x86a&instance_name=demo&instance_type=small_b&login_mode=passwd&login_passwd=QingCloud20130712&signature_method=HmacSHA256&signature_version=1&time_stamp=2013-08-27T14%3A30%3A10Z&version=1&vxnets.1=vxnet-0&zone=pek3a";
+        String strToSignOrigin = "GET\\n/iaas/\\naccess_key_id=QYACCESSKEYIDEXAMPLE&action=RunInstances&count=1&image_id=centos64x86a&instance_name=demo&instance_type=small_b&login_mode=passwd&login_passwd=QingCloud20130712&signature_method=HmacSHA256&signature_version=1&time_stamp=2013-08-27T14%3A30%3A10Z&version=1&vxnets.1=vxnet-0&zone=pek3a";
         byte[] h = Sha256Utils.hash(strToSign.getBytes(StandardCharsets.UTF_8));
         String sign = Base64Utils.encode(Arrays.toString(h));
         return UrlEncodeUtils.encodeUrl(sign);
